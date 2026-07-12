c0a9a10b1b1048c1d1a514b2cae2b9c1f10f30fd
KristinaAlekseenko <22kristina987@gmail.com>
2026-07-12 09:01:38 +0300

    task04

---
+++ task04/Cruiser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        public int Speed => 50;
+        public int FirePower => 100;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Вперед со скоростью {Speed}");
+        }
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"Поворот на {angle}");
+        }
+        public void Fire()
+        {
+            Console.WriteLine($"огонь с мощностью {FirePower}");
+        }
+    }
+}
---
+++ task04/Fighter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task04
+{
+    public class Fighter : ISpaceship
+    {
+        public int Speed => 100;
+        public int FirePower => 70;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Вперед со скоростью {Speed}");
+        }
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"Поворот на {angle}");
+        }
+        public void Fire()
+        {
+            Console.WriteLine($"огонь с мощностью {FirePower}");
+        }
+    }
+
+}
