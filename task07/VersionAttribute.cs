f58a0d99e904186f93e66e9230b6a81ab1249600
KristinaAlekseenko <22kristina987@gmail.com>
2026-07-12 09:26:14 +0300

    task07

---
+++ task07/VersionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task07
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class VersionAttribute : Attribute
+    {
+        public int _major { get; }
+
+        public int _minor { get; }
+
+
+        public VersionAttribute(int major, int minor)
+        {
+            _major = major;
+
+            _minor = minor;
+
+        }
+    }
+}
