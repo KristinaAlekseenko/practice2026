68a4a029cefd6757c933e27b92711cfa49d0454d
KristinaAlekseenko <22kristina987@gmail.com>
2026-07-12 09:42:10 +0300

    task08

---
+++ FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLib;
+
+namespace FileSystemCommands
+{
+    public class FindFilesCommand : ICommand
+    {
+        private readonly string DirPath;
+        private readonly string SearchPattern;
+        public List<string> FoundFiles { get; private set; } = new();
+
+        public FindFilesCommand(string dirPath, string searchPattern)
+        {
+            DirPath = dirPath;
+            SearchPattern = searchPattern;
+        }
+
+        public void Execute()
+        {
+            if (!Directory.Exists(DirPath))
+            {
+                Console.WriteLine($"Директория не найдена: {DirPath}");
+                return;
+            }
+
+            FoundFiles = Directory.GetFiles(DirPath, SearchPattern).ToList();
+
+            Console.WriteLine($"Маска {SearchPattern}");
+
+            foreach (var file in FoundFiles)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+        }
+    }
+}
